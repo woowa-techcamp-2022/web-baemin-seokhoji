0694b36bf19c871f56a503538f7ac6d9e72be916
Seokho Ji <camp_seokoji97@woowafriends.com>
2022-07-06 22:42:14 +0900

    chore: classes -> function 문법 변경

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
-import App from './app';
-
-const app = new App().app;
+import app from './app';
 
 app.listen(3000, () => {
-	console.log('Server listening on port 3000');
+	console.log('실행');
 });
