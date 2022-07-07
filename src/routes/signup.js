57b944a8e40c14c0365d0e5fdb9ba05fb873aaf8
Seokho Ji <camp_seokoji97@woowafriends.com>
2022-07-08 03:32:57 +0900

    feat: :sparkles: 회원가입 로직 생성

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -1,28 +1,43 @@
 const { Router } = require('express');
+const bcrypt = require('bcrypt');
+
 const db = require('../utils/db');
 
 const router = Router();
 
+const HASH_SALT = 13;
+
 router.post('/', async (req, res) => {
-	const body = req.body;
+	const { email, password } = req.body;
 
-	const data = await db.read();
+	const exUser = await db.get('user').find({ email }).value();
 
-	console.log(db.read());
+	if (exUser) return res.redirect('/signup/detail?error=이미 사용중인 이메일입니다!');
 
-	res.render('detail', { email: null });
+	const hashedPassword = await bcrypt.hash(password, HASH_SALT);
+
+	const user = {
+		...req.body,
+		password: hashedPassword,
+	};
+
+	db.get('user').push(user).write();
+
+	return res.redirect(302, '/');
 });
 
 router.get('/agree', (_, res) => {
-	res.render('agree', { title: 'Test', message: 'test message' });
+	res.render('agree');
 });
 
 router.get('/phone', (_, res) => {
-	res.render('phone', { title: 'Test', message: 'test message' });
+	res.render('phone');
 });
 
-router.get('/detail', (req, res) => {
-	res.render('detail', { email: null });
+router.get('/detail', async (req, res) => {
+	const { error } = req.query;
+
+	res.render('detail', { error });
 });
 
 module.exports = router;
