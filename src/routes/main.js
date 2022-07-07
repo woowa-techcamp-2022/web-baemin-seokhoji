c129d045d11edb2b861931a7a370bceb27d4eae6
Seokho Ji <camp_seokoji97@woowafriends.com>
2022-07-08 03:32:36 +0900

    feat: :sparkles: session 정보가 존재할 경우 데이터 전송 추가

--- src/routes/main.js
+++ src/routes/main.js
@@ -2,7 +2,9 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res, next) => {
-	res.render('index', {});
+	const { nickname } = req.session;
+
+	res.render('index', { nickname });
 });
 
 module.exports = router;
