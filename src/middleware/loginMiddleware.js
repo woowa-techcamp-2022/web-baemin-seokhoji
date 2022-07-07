0f149bf68920fd7aaca6e01bab39379e411d0b3d
Seokho Ji <camp_seokoji97@woowafriends.com>
2022-07-08 03:30:38 +0900

    feat: :sparkles: session이 존재하지 않을때 index page로 redirect

--- src/middleware/loginMiddleware.js
+++ src/middleware/loginMiddleware.js
@@ -2,14 +2,15 @@ exports.isLoggedIn = (req, res, next) => {
 	if (req.session.nickname) {
 		next();
 	} else {
-		next('로그인이 필요합니다.');
+		res.redirect('/');
 	}
 };
 
 exports.isNotLoggedIn = (req, res, next) => {
+	console.log(req.session);
 	if (!req.session.nickname) {
 		next();
 	} else {
-		next('로그인하지 않은 사용자만 접근 가능합니다.');
+		res.redirect('/');
 	}
 };
